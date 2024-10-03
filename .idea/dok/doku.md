dfa9448c9f536614b431baff7002dbb16a8b6499
Lex0007 <alklapeer@tsn.at>
2024-10-03 13:46:17 +0200

    Neue Klassen hinzugefügt

---
+++ .idea/dok/doku.md
@@ -0,0 +1,5 @@
+# Modul 3
+
+## Klasse Medium erstellt
+
+Diese Klasse erstellt, dass ich keine doppelten Daten in den Klassen eBooks und Filme habe.
---
+++ src/Medium.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class Medium {
+    private String titel;
+    private String kommentar;
+    private Date erscheinungsdatum;
+    private String genre;
+
+    public Medium(String titel, String kommentar, Date erscheinungsdatum, String genre) {
+        this.titel = titel;
+        this.kommentar = kommentar;
+        this.erscheinungsdatum = erscheinungsdatum;
+        this.genre = genre;
+    }
+
+
+}
